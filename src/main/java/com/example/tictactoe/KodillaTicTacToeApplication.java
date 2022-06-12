6ef5cb8465b378823e9d49b76c05968e7651f05b
antykom <36765862+antykom@users.noreply.github.com>
2022-06-12 14:17:18 +0200

    TicTacToe programm started

---
+++ src/main/java/com/example/tictactoe/KodillaTicTacToeApplication.java
@@ -0,0 +1,13 @@
+package com.example.tictactoe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaTicTacToeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaTicTacToeApplication.class, args);
+    }
+
+}
